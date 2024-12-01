e345dce22633c692a90527dae8c922e0975b43eb
HyeonMin Jeon <fudrim06@gmail.com>
2024-12-01 15:41:35 +0900

    Clean up artifacts and update hash files
    Deleted multiple unused artifact files to optimize storage and improve project efficiency. Also updated several .bhc hash files in the BurstCache JIT directory to reflect recent changes. This maintenance should enhance system performance and clarity of the codebase.

--- Assets/Scripts/Windows/Setting/ResolutionController.cs
+++ Assets/Scripts/Windows/Setting/ResolutionController.cs
@@ -18,37 +18,46 @@ namespace Windows.Setting
         [SerializeField, Tooltip("스크린 모드를 지정하는 변수")]
         private ScreenMode screenMode;
 
-        [SerializeField, Tooltip("스크린 사이즈를 지정하는 변수")]
-        private Vector2 resolutionSize = new Vector2(1920, 1080);
-        
-        private void Awake()
-        {
-            SetScreenMode();
-        }
+		[SerializeField, Tooltip("스크린 사이즈를 지정하는 변수")]
+		private Vector2 resolutionSize;
 
-        private void SetScreenMode()
-        {
-            FullScreenMode unityScreenMode;
-
-            switch (screenMode)
-            {
-                case ScreenMode.ExclusiveFullScreen:
-                    unityScreenMode = FullScreenMode.ExclusiveFullScreen;
-                    break;
-                case ScreenMode.FullScreenWindow:
-                    unityScreenMode = FullScreenMode.FullScreenWindow;
-                    break;
-                case ScreenMode.MaximizedWindow:
-                    unityScreenMode = FullScreenMode.MaximizedWindow;
-                    break;
-                case ScreenMode.Windowed:
-                    unityScreenMode = FullScreenMode.Windowed;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            Screen.SetResolution((int)resolutionSize.x, (int)resolutionSize.y, unityScreenMode);
-        }
+		private void Update()
+		{
+			ApplyScreenSettingMethod();
+		}
+		
+		private void ApplyScreenSettingMethod()
+		{
+			switch (screenMode)
+			{
+				case ScreenMode.ExclusiveFullScreen:
+					if (Screen.fullScreenMode != FullScreenMode.ExclusiveFullScreen)
+					{
+						Screen.SetResolution((int) resolutionSize.x, (int) resolutionSize.y, FullScreenMode.ExclusiveFullScreen);
+					}
+					break;
+				case ScreenMode.FullScreenWindow:
+					if (Screen.fullScreenMode != FullScreenMode.FullScreenWindow)
+					{
+						Screen.SetResolution((int) resolutionSize.x, (int) resolutionSize.y, FullScreenMode.FullScreenWindow);
+					}
+					break;
+				case ScreenMode.MaximizedWindow:
+					if (Screen.fullScreenMode != FullScreenMode.MaximizedWindow)
+					{
+						Screen.SetResolution((int) resolutionSize.x, (int) resolutionSize.y, FullScreenMode.MaximizedWindow);
+						
+					}
+					break;
+				case ScreenMode.Windowed:
+					if (Screen.fullScreenMode != FullScreenMode.Windowed)
+					{
+						Screen.SetResolution((int)resolutionSize.x, (int)resolutionSize.y, FullScreenMode.Windowed);
+					}
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
     }
 }
